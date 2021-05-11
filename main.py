1750616479c299540a2051d96676a64d7d1ad48a
tosh7 <zlia.6.lj.425@gmail.com>
2021-05-11 17:03:45 +0900

    successed with loading assets files

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ import yaml
 def main():
     with open('config/directory.yml') as yml:
         config = yaml.safe_load(yml)['directory']
-        old_assets = yaml.safe_load(yml)['oldAssets']
-        new_assets = yaml.safe_load(yml)['newAssets']
+    with open('config/directory.yml') as yml:
+        old_assets = yaml.safe_load(yml)['old_assets']
+    with open('config/directory.yml') as yml:
+        new_assets = yaml.safe_load(yml)['new_assets']
 
+    print(old_assets)
+    print(new_assets)
     storyboard_directories = glob.glob(f"{config}**/*.storyboard", recursive=True)
     xib_dictionaries = glob.glob(f"{config}**/*.xib", recursive=True)
+
+    
 
 main()
