05b0f0a0a9319085338d7319ddf18d0c5c17cf93
tosh7 <zlia.6.lj.425@gmail.com>
2021-05-11 17:42:01 +0900

    got hex data, but sometimes index got out of range

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import glob
 import yaml
 import os
+import re
 
 def main():
     with open('config/directory.yml') as yml:
@@ -18,7 +19,19 @@ def main():
     old_color_sets = glob.glob(f"{old_assets}/*.colorset")
     
     for old_color in old_color_sets:
-        // print out color names
+        # print out color names
         print(os.path.splitext(os.path.basename(old_color))[0])
+
+        contents_json = open((f"{old_color}/Contents.json"))
+        for line in contents_json.readlines():
+            if 'red' in line:
+                red = re.findall('0x(.*)"', line)[0]
+                print(red)
+            elif 'blue' in line:
+                red = re.findall('0x(.*)"', line)[0]
+                print(red)
+            elif 'green' in line:
+                red = re.findall('0x(.*)"', line)[0]
+                print(red)
 
 main()
